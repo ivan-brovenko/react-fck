c8340272f150edcec9a991ac262f7a249e7819d2
Ivan Brovenko <ivan_brovenko@epam.com>
2019-12-12 17:45:43 +0200

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import '../scss/main.scss';
 
 class App extends React.Component {
     render(){
         return (
             <div>
-                <h1>666</h1>
+                <Header/>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,17 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader', 'sass-loader']
             }
         ]
+    },
+    devServer: {
+        overlay: true
     },
     plugins: [
         new HtmlWebpackPlugin({
